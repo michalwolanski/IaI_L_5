2964fa03a6a6c0bd57e01c1f9d600ef18aef1194
michalwolanski <72569560+michalwolanski@users.noreply.github.com>
2020-11-04 22:47:18 +0100

    nawigacja

---
+++ script.js
@@ -0,0 +1,3 @@
+let nav = '<nav><a href="index.html">HOME</a>'+'<nav><a href="contact.html">KONTAKT</a>'+'<a href="mapy.html">MAPA</a>'+'<a href=#3>BLOG</a>'+'<a href=#4>QUIZ</a></nav>';
+let elem = document.querySelector('.navbar');
+elem.innerHTML = nav
