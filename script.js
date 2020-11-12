a7b7727a6f495a2fb181e0813274f0743c413e87
michalwolanski <72569560+michalwolanski@users.noreply.github.com>
2020-11-12 22:00:14 +0100

    scroll

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 let nav = '<nav><a href="index.html">HOME</a>'+'<nav><a href="contact.html">KONTAKT</a>'+'<a href="mapy.html">MAPA</a>'+'<a href=#3>BLOG</a>'+'<a href=#4>QUIZ</a></nav>';
 let elem = document.querySelector('.navbar');
 elem.innerHTML = nav
+
+//pobranie referencji do przycisku
+let button = document.querySelector('#bg');
+
+//załączenie nasłuchiwacza
+button.addEventListener('click', ()=> {
+  //pobranie referencji do tła strony
+  let body = document.getElementsByTagName('body')[0];
+
+  //zmiana tła strony - losowanie kolorów
+  body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+});
